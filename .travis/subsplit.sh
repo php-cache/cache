83abed29f43fd088b4373d5fe90c21a0a12f05cc
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-31 20:10:46 +0100

    Added SessionHandler, MongoDB and Filesystem adapters

--- .travis/subsplit.sh
+++ .travis/subsplit.sh
@@ -4,11 +4,12 @@ git clone https://github.com/dflydev/git-subsplit.git
 cd git-subsplit && sudo sh ./install.sh && cd ..
 
 # Init the subplit
-git subsplit init https://github.com/subtree-test/main.git
+git subsplit init https://github.com/php-cache/cache.git
 git subsplit update
 
 FLAGS="--heads=master --no-tags"
 # Push the changes to other repositories
-time git subsplit publish src/Cache/adapter-bar:git@github.com:subtree-test/adapter-bar.git $FLAGS
-time git subsplit publish src/Cache/adapter-foo:git@github.com:subtree-test/adapter-foo.git $FLAGS
-time git subsplit publish src/Cache/foobar-common:git@github.com:subtree-test/foobar-common.git $FLAGS
+git subsplit publish src/Cache/Adapter/Filesystem:git@github.com:php-cache/filesystem-adapter.git $FLAGS
+git subsplit publish src/Cache/Adapter/MongoDB:git@github.com:php-cache/mongodb-adapter.git $FLAGS
+git subsplit publish src/Cache/SessionHandler:git@github.com:php-cache/session-handler.git $FLAGS
+
