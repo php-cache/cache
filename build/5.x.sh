5be665037092e3c437e6ebb6d8d1f073525c5a81
Aaron Scherer <aequasi@gmail.com>
2016-02-01 11:51:46 -0800

    FIxing build

--- build/5.x.sh
+++ build/5.x.sh
@@ -2,3 +2,6 @@
 
 # Install APC Adapter & APCu Adapter dependencies
 yes '' | pecl install apcu-4.0.8
+
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
--- build/all.sh
+++ build/all.sh
@@ -10,8 +10,5 @@ if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./build/php.ini; fi
 pip install --user codecov
 
 # Enable extension
-echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
-echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
