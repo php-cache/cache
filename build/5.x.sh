e183690704432cb559db54bda3d2bca110ac11f9
Aaron Scherer <aequasi@gmail.com>
2016-02-01 01:33:15 -0800

    Fixing 5

--- build/5.x.sh
+++ build/5.x.sh
@@ -5,4 +5,5 @@ echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.i
 yes '' | pecl install apcu-4.0.8
 
 # Install Mongo
+echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
 echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
