c55fd73c118aea65b751b9526e4c8a26c60c6952
Aaron Scherer <aequasi@gmail.com>
2016-02-01 01:46:33 -0800

    Fixing build

--- build/5.x.sh
+++ build/5.x.sh
@@ -4,5 +4,3 @@
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
 yes '' | pecl install apcu-4.0.8
 
-# Install Mongo
-yes '' | pecl install mongodb-1.1.0
--- build/all.sh
+++ build/all.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 # Disable xdebug when hhvm or when SEND_COVERAGE is false
-if [ $TRAVIS_PHP_VERSION != hhvm && $SEND_COVERAGE != true ]; then phpenv config-rm xdebug.ini; fi
+if [ $TRAVIS_PHP_VERSION != hhvm && $TRAVIS_PHP_VERSION != '7.0' ]; then phpenv config-rm xdebug.ini; fi
 
 # Doing something with phpenv
-if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./tests/travis/php.ini; fi
+if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./build/php.ini; fi
 
 # Install codecov
 pip install --user codecov
 
-# Install Memcached extension
-if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi;
-if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi;
-
-# Install Redis extension
+# Enable extension
 mkdir -p ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
