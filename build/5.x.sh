f3cd318b90946acde122176e876064e112eef832
Aaron Scherer <aequasi@gmail.com>
2016-02-01 00:50:14 -0800

    gs

--- build/5.x.sh
+++ build/5.x.sh
@@ -2,3 +2,6 @@
 
 # Install APC Adapter & APCu Adapter dependencies
 yes '' | pecl install apcu-4.0.8
+
+# Install Mongo
+echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
--- build/all.sh
+++ build/all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Disable xdebug when hhvm or when SEND_COVERAGE is false
-if [[ $TRAVIS_PHP_VERSION != hhvm && $SEND_COVERAGE != true ]]; then phpenv config-rm xdebug.ini; fi
+if [ $TRAVIS_PHP_VERSION != hhvm && $SEND_COVERAGE != true ]; then phpenv config-rm xdebug.ini; fi
 
 # Doing something with phpenv
 if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./tests/travis/php.ini; fi
@@ -9,12 +9,9 @@ if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./tests/travis/php.
 # Install codecov
 pip install --user codecov
 
-# Install Mongo
-./build/installMongo.sh
-
 # Install Memcached extension
-if [[ $TRAVIS_PHP_VERSION != 'hhvm' ]]; then echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi;
-
+if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi;
+if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini; fi;
 
 # Install Redis extension
 mkdir -p ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d
