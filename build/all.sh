741919ded3cd10a37bb7a642e96d297ab1a76a78
Aaron Scherer <aequasi@gmail.com>
2016-02-01 22:11:31 -0800

    Update all.sh

--- build/all.sh
+++ build/all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Disable xdebug when hhvm or when SEND_COVERAGE is false
-if [ $TRAVIS_PHP_VERSION != hhvm && $TRAVIS_PHP_VERSION != '7.0' ]; then phpenv config-rm xdebug.ini; fi
+# Disable xdebug when version isn't 7.0
+if [ $TRAVIS_PHP_VERSION != '7.0' ]; then phpenv config-rm xdebug.ini; fi
 
 # Doing something with phpenv
 if [ $TRAVIS_PHP_VERSION != 'hhvm' ]; then phpenv config-add ./build/php.ini; fi
