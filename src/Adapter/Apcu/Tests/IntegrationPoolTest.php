1d0daaa7ec15784efe604fc45450ee70b75b1155
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-02-02 13:34:56 +0100

    Skip expiration tests

--- src/Adapter/Apcu/Tests/IntegrationPoolTest.php
+++ src/Adapter/Apcu/Tests/IntegrationPoolTest.php
@@ -16,6 +16,10 @@ use Cache\IntegrationTests\CachePoolTest as BaseTest;
 
 class IntegrationPoolTest extends BaseTest
 {
+    protected $skippedTests = [
+        'testExpiration' => 'The cache expire at the next request.',
+    ];
+
     public function createCachePool()
     {
         if (defined('HHVM_VERSION') || !function_exists('apcu_store')) {
