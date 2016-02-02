7e5d54b5c1f03d63e917db643e83dfc64391f790
tgabi333 <tgabi333@gmail.com>
2016-02-02 09:58:25 +0100

    apc tests for php7 with apc_bc apc ttl fix in command line

--- build/5.x.sh
+++ build/5.x.sh
@@ -2,5 +2,5 @@
 
 # Install APC Adapter & APCu Adapter dependencies
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini;
-yes '' | pecl install apcu-4.0.8
+yes '' | pecl install apcu-4.0.10
 
--- build/7.x.sh
+++ build/7.x.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Install APC Adapter & APCu Adapter dependencies
-yes '' | pecl install apcu-5.1.0
+yes '' | pecl install apcu
+
+yes '' | pecl install apcu_bc-beta
 
 # Install memcache(d)
 yes '' | pecl install memcache memcached
