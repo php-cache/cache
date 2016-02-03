219fa6d1692a506ece3c92e0a5b27bc1f6776825
Aaron Scherer <aequasi@gmail.com>
2016-02-02 18:19:39 -0800

    Testing

---
+++ build/php/5.6/Redis.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Install redis"
+yes '' | pecl install redis
--- build/runTest.sh
+++ build/runTest.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-SLUG=$(grep -Pho 'cache/[A-Za-z-\.]+' $1/composer.json | xargs | awk '{print $1}')
-printf "\n\n************ Running tests for $SLUG ************\n\n"
-
 cd $1
+SLUG=$(grep -Pho 'cache/[A-Za-z-\.]+' composer.json | xargs | awk '{print $1}')
+
+printf "\n\n************ Opening directory: $1 ************\n"
+printf     "************ Running tests for: $SLUG ************\n\n"
+
+
+if [ ! -z "$BUILD_ALL" ]; then composer require --no-update mongodb/mongodb:^1.0; fi
 if [ $TRAVIS_PHP_VERSION = 5.5 ]; then composer require --no-update phpunit/phpunit:~4.0; fi
-composer install --no-interaction --prefer-source --ignore-platform-reqs
+composer install --no-interaction --prefer-source
 
-TEST="./vendor/bin/phpunit $2"
+TEST="./vendor/bin/phpunit"
+if [ ! -z "$BUILD_ALL" ]; then TEST="$TEST --coverage-clover=coverage.xml"; fi
 
-printf "On $SLUG\n"
-printf "Command: $TEST\n\n"
 if [ "$TRAVIS_PHP_VERSION" == '7.0' ]
 then
     phpdbg -qrr $TEST
