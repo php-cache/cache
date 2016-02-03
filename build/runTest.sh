ee9b4e34ae6a954ed33bda7007c4c066d9b49526
Aaron Scherer <aequasi@gmail.com>
2016-02-02 23:01:39 -0800

    Testing

--- build/runTest.sh
+++ build/runTest.sh
@@ -10,8 +10,8 @@ printf     "************ Running tests for: $SLUG ************\n\n"
 if [ ! -z "$BUILD_ALL" ]; then composer require --no-update mongodb/mongodb:^1.0; fi
 composer install --no-interaction
 
-TEST="./vendor/bin/paratest -p 6 -f"
-if [ ! -z "$BUILD_ALL" ]; then TEST="$TEST --coverage-clover=coverage.xml"; fi
+TEST="./vendor/bin/paratest -p 6 --runner WrapperRunner"
+if [[ ! -z "$BUILD_ALL" && "$TRAVIS_PHP_VERSION" == '7.0' ]]; then TEST="$TEST --coverage-clover=coverage.xml"; fi
 
 if [ "$TRAVIS_PHP_VERSION" == '7.0' ]
 then
