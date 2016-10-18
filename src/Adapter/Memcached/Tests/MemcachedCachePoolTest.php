f8e73eb3f98c494eec1151a0f0e64759cf593dbe
Joost Pastoor <joost.pastoor@munisense.com>
2016-10-18 10:53:11 +0200

    Fixed issue where Memcached expects an absolute TTL timestamp when value is more than 30 days

---
+++ src/Adapter/Memcached/Tests/MemcachedCachePoolTest.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Cache\Adapter\Memcached\Tests;
+
+
+use Cache\Adapter\Common\CacheItem;
+use Cache\Adapter\Memcached\MemcachedCachePool;
+
+class MemcachedCachePoolTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Ensures that TTL larger than 30 days are sent as absolute timestamps
+     * https://github.com/memcached/memcached/wiki/Programming#expiration
+     */
+    public function testTimeToLiveMoreThan30days()
+    {
+        /** @var \Memcached|\PHPUnit_Framework_MockObject_MockObject $cache */
+        $cache = $this->getMockBuilder(\Memcached::class)->disableOriginalConstructor()->getMock();
+        $cache->expects($this->once())->method("set")->with(
+            $this->equalTo("a"),
+            $this->anything(),
+            // Test that the TTL timestamp is absolute (greaterThanOrEqual to account for small timing)
+            $this->greaterThanOrEqual(time() + (86400 * 365))
+        )->willReturn(true);
+
+        $pool = new MemcachedCachePool($cache);
+
+        $cache_item = new CacheItem("a", false, "1");
+        
+        // Input relative expiration time of 1 year
+        $cache_item->expiresAfter(86400 * 365);
+        $pool->save($cache_item);
+    }
+
+    /**
+     * Ensures that TTL less than 30 days are still sent as relative timestamps
+     */
+    public function testTimeToLiveLessThan30days()
+    {
+        /** @var \Memcached|\PHPUnit_Framework_MockObject_MockObject $cache */
+        $cache = $this->getMockBuilder(\Memcached::class)->disableOriginalConstructor()->getMock();
+        $cache->expects($this->once())->method("set")->with(
+            $this->equalTo("b"),
+            $this->anything(),
+            // Test that the TTL timestamp is relative
+            $this->equalTo(86400 * 20)
+        )->willReturn(true);
+
+        $pool = new MemcachedCachePool($cache);
+
+        $cache_item = new CacheItem("b", false, "1");
+
+        // Input relative expiration time of 20 days
+        $cache_item->expiresAfter(86400 * 20);
+        $pool->save($cache_item);
+    }
+}
