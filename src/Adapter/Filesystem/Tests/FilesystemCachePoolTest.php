e2e51811aaf6ba64129ea899035897ad3ba20366
Guy Radford <gradford@arinc.com>
2016-11-02 10:27:04 +0000

    updated tests code style

--- src/Adapter/Filesystem/Tests/FilesystemCachePoolTest.php
+++ src/Adapter/Filesystem/Tests/FilesystemCachePoolTest.php
@@ -21,22 +21,22 @@ class FilesystemCachePoolTest extends \PHPUnit_Framework_TestCase
 
     public function testCleanupOnExpire()
     {
-        $cacheKey = 'test_ttl_null';
-        $cacheFilename = $filename = str_replace('=', '_', base64_encode($cacheKey));
-        
+        $cacheKey      = 'test_ttl_null';
+        $cacheFilename = str_replace('=', '_', base64_encode($cacheKey));
+
         $pool = $this->createCachePool();
-        
+
         $item = $pool->getItem($cacheKey);
         $item->set('data');
         $item->expiresAt(new \DateTime('now'));
         $pool->save($item);
-        $this->assertTrue($this->getFilesystem()->has('cache/' . $cacheFilename));
-        
+        $this->assertTrue($this->getFilesystem()->has('cache/'.$cacheFilename));
+
         sleep(1);
-        
+
         $item = $pool->getItem($cacheKey);
         $this->assertFalse($item->isHit());
-        $this->assertFalse($this->getFilesystem()->has('cache/' . $cacheFilename));
+        $this->assertFalse($this->getFilesystem()->has('cache/'.$cacheFilename));
     }
 
     public function testChangeFolder()
@@ -45,6 +45,6 @@ class FilesystemCachePoolTest extends \PHPUnit_Framework_TestCase
         $pool->setFolder('foobar');
 
         $pool->save($pool->getItem('test_path'));
-        $this->assertTrue($this->getFilesystem()->has('foobar/' . str_replace('=', '_', base64_encode('test_path'))));
+        $this->assertTrue($this->getFilesystem()->has('foobar/'.str_replace('=', '_', base64_encode('test_path'))));
     }
 }
