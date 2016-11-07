b951e24a481ce07db43e62eba3e8928977ba3e74
Daniel Bannert <d.bannert@anolilab.de>
2016-11-07 23:50:34 +0100

    added cs fixes

--- src/Encryption/EncryptedItemDecorator.php
+++ src/Encryption/EncryptedItemDecorator.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of php-cache organization.
  *
- * (c) 2015-2016 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -36,7 +36,7 @@ class EncryptedItemDecorator implements CacheItemInterface, HasExpirationDateInt
 
     /**
      * @param CacheItemInterface $cacheItem
-     * @param Key                    $key
+     * @param Key                $key
      */
     public function __construct(CacheItemInterface $cacheItem, Key $key)
     {
