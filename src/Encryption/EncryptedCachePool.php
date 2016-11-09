3e1383109547a11d7598194daf3f38ddab42b14f
Daniel Bannert <d.bannert@anolilab.de>
2016-11-09 22:32:20 +0100

    [WIP] Adding encryption decorator (#107)
    * Adding encrypted decorator for psr6
    
    * fixed testItemModifiersReturnsStatic
    
    * rename classes | added cs fixes | remove old readme lines
    
    * Rename folder | fix phpdoc in EncryptedItemDecorator
    
    * fix last test
    
    * added cs fixes
    
    * update php header
    
    * added a Changelog
    
    * fix format

---
+++ src/Encryption/EncryptedCachePool.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015-2016 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Cache\Encryption;
+
+use Defuse\Crypto\Key;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Wrapps a CacheItemInterface with EncryptedItemDecorator.
+ *
+ * @author Daniel Bannert <d.bannert@anolilab.de>
+ */
+class EncryptedCachePool implements CacheItemPoolInterface
+{
+    /**
+     * @type CacheItemPoolInterface
+     */
+    private $cachePool;
+
+    /**
+     * @type Key
+     */
+    private $key;
+
+    /**
+     * @param CacheItemPoolInterface $cachePool
+     * @param Key                    $key
+     */
+    public function __construct(CacheItemPoolInterface $cachePool, Key $key)
+    {
+        $this->cachePool = $cachePool;
+        $this->key       = $key;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItem($key)
+    {
+        $item = $this->cachePool->getItem($key);
+
+        if (!($item instanceof EncryptedItemDecorator)) {
+            return new EncryptedItemDecorator($item, $this->key);
+        }
+
+        return $item;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems(array $keys = [])
+    {
+        return array_map(function (CacheItemInterface $inner) {
+            if (!($inner instanceof EncryptedItemDecorator)) {
+                return new EncryptedItemDecorator($inner, $this->key);
+            }
+
+            return $inner;
+        }, $this->cachePool->getItems($keys));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasItem($key)
+    {
+        return $this->cachePool->hasItem($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        return $this->cachePool->clear();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteItem($key)
+    {
+        return $this->cachePool->deleteItem($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteItems(array $keys)
+    {
+        return $this->cachePool->deleteItems($keys);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(CacheItemInterface $item)
+    {
+        if (!($item instanceof EncryptedItemDecorator)) {
+            $item = new EncryptedItemDecorator($item, $this->key);
+        }
+
+        return $this->cachePool->save($item);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function saveDeferred(CacheItemInterface $item)
+    {
+        if (!($item instanceof EncryptedItemDecorator)) {
+            $item = new EncryptedItemDecorator($item, $this->key);
+        }
+
+        return $this->cachePool->saveDeferred($item);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function commit()
+    {
+        return $this->cachePool->commit();
+    }
+}
---
+++ src/Encryption/Tests/IntegrationPoolTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015-2016 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Cache\Encryption\Tests;
+
+use Cache\Adapter\PHPArray\ArrayCachePool;
+use Cache\Encryption\EncryptedCachePool;
+use Cache\IntegrationTests\CachePoolTest;
+use Defuse\Crypto\Key;
+
+class IntegrationPoolTest extends CachePoolTest
+{
+    private $cacheArray = [];
+
+    public function createCachePool()
+    {
+        return new EncryptedCachePool(
+            new ArrayCachePool(null, $this->cacheArray),
+            Key::loadFromAsciiSafeString('def000007c57b06c65b0df4bcac939924e42605d8d76e1462b619318bf94107c28db30c5394b4242db5e45563e1226cffcdff8123fa214ea1fcc4aa10b0ddb1b4a587b7e')
+        );
+    }
+}
