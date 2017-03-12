0b9ddca57132651afcd28e23924cd57d1fd3e51a
florianv <voutzinos.florian@gmail.com>
2017-03-12 15:36:32 +0100

    Added PSR-6 to Illuminate adapter

---
+++ src/Adapter/Illuminate/Tests/IlluminateAdapterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\Adapter\Illuminate\Tests;
+
+use Cache\Adapter\Common\CacheItem;
+use Cache\Adapter\Illuminate\IlluminateCachePool;
+use Illuminate\Contracts\Cache\Store;
+use Mockery as m;
+use Mockery\MockInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+class IlluminateAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @type IlluminateCachePool
+     */
+    private $pool;
+
+    /**
+     * @type MockInterface|CacheItem
+     */
+    private $mockItem;
+
+    /**
+     * @type MockInterface|Store
+     */
+    private $mockStore;
+
+    protected function setUp()
+    {
+        $this->mockItem  = m::mock(CacheItem::class);
+        $this->mockStore = m::mock(Store::class);
+        $this->pool      = new IlluminateCachePool($this->mockStore);
+    }
+
+    public function testConstructor()
+    {
+        $this->assertInstanceOf(IlluminateCachePool::class, $this->pool);
+        $this->assertInstanceOf(CacheItemPoolInterface::class, $this->pool);
+    }
+}
