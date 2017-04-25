1a4f62948e2b319276362aa345b1a5489f6a1f11
Josef Panák <josef.panak@ringieraxelspringer.sk>
2017-04-25 21:05:39 +0200

    added test for CachePoolChain

---
+++ src/Adapter/Chain/Tests/CachePoolChainTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Cache\Adapter\Chain\Tests;
+
+use Cache\Adapter\Chain\CachePoolChain;
+use Cache\Adapter\Common\CacheItem;
+use Cache\Adapter\PHPArray\ArrayCachePool;
+
+/**
+ * Class ChainPoolTest
+ * @package Cache\Adapter\Chain\Tests
+ */
+class CachePoolChainTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetItemStoreToPrevious()
+    {
+        $firstPool = new ArrayCachePool();
+        $secondPool = new ArrayCachePool();
+        $chainPool = new CachePoolChain([$firstPool, $secondPool]);
+
+        $key = 'test_key';
+        $item = new CacheItem($key, true, 'value');
+        $item->expiresAfter(60);
+        $secondPool->save($item);
+
+        $loadedItem = $firstPool->getItem($key);
+        $this->assertFalse($loadedItem->isHit());
+
+        $loadedItem = $secondPool->getItem($key);
+        $this->assertTrue($loadedItem->isHit());
+
+        $loadedItem = $chainPool->getItem($key);
+        $this->assertTrue($loadedItem->isHit());
+
+        $loadedItem = $firstPool->getItem($key);
+        $this->assertTrue($loadedItem->isHit());
+    }
+
+    public function testGetItemsStoreToPrevious()
+    {
+        $firstPool = new ArrayCachePool();
+        $secondPool = new ArrayCachePool();
+        $chainPool = new CachePoolChain([$firstPool, $secondPool]);
+
+        $key = 'test_key';
+        $item = new CacheItem($key, true, 'value');
+        $item->expiresAfter(60);
+        $secondPool->save($item);
+        $firstExpirationTime = $item->getExpirationTimestamp();
+
+        $key2 = 'test_key2';
+        $item = new CacheItem($key2, true, 'value2');
+        $item->expiresAfter(60);
+        $secondPool->save($item);
+        $secondExpirationTime = $item->getExpirationTimestamp();
+
+        $loadedItem = $firstPool->getItem($key);
+        $this->assertFalse($loadedItem->isHit());
+
+        $loadedItem = $firstPool->getItem($key2);
+        $this->assertFalse($loadedItem->isHit());
+
+        $loadedItem = $secondPool->getItem($key);
+        $this->assertTrue($loadedItem->isHit());
+
+        $loadedItem = $secondPool->getItem($key2);
+        $this->assertTrue($loadedItem->isHit());
+
+        $items = $chainPool->getItems([$key, $key2]);
+
+        $this->assertArrayHasKey($key, $items);
+        $this->assertArrayHasKey($key2, $items);
+
+        $this->assertTrue($items[$key]->isHit());
+        $this->assertTrue($items[$key2]->isHit());
+
+        $loadedItem = $firstPool->getItem($key);
+        $this->assertTrue($loadedItem->isHit());
+        $this->assertEquals($firstExpirationTime, $loadedItem->getExpirationTimestamp());
+
+        $loadedItem = $firstPool->getItem($key2);
+        $this->assertTrue($loadedItem->isHit());
+        $this->assertEquals($secondExpirationTime, $loadedItem->getExpirationTimestamp());
+    }
+}
