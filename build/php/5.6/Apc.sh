25f944844a1e91e049db6c41eae0a4cb327cbae7
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-16 17:09:31 +0200

    Reduce sleep

--- build/php/5.6/Apc.sh
+++ build/php/5.6/Apc.sh
@@ -4,4 +4,4 @@ echo "Add php.ini settings"
 phpenv config-add ./build/php/apc.ini
 
 echo "Install APC Adapter & APCu Adapter dependencies"
-yes '' | pecl install -f apcu-4.0.10
+yes '' | pecl install -f apcu-4.0.11
