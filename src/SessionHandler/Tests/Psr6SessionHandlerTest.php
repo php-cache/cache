826bbac816b51dd6667f78e11075cd609f356928
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-16 23:15:07 +0200

    [Session] cleanup and minor fixes (#180)
    * MInor cleanup and added tests
    
    * Removed depedencies
    
    * Applied changes from StyleCI

--- src/SessionHandler/Tests/Psr6SessionHandlerTest.php
+++ src/SessionHandler/Tests/Psr6SessionHandlerTest.php
@@ -11,44 +11,37 @@
 
 namespace Cache\SessionHandler\Tests;
 
+use Cache\Adapter\PHPArray\ArrayCachePool;
 use Cache\SessionHandler\Psr6SessionHandler;
-use Mockery as m;
 use Psr\Cache\CacheItemInterface;
 use Psr\Cache\CacheItemPoolInterface;
 
 /**
  * @author Aaron Scherer <aequasi@gmail.com>
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
 class Psr6SessionHandlerTest extends \PHPUnit_Framework_TestCase
 {
+    const TTL    = 100;
+    const PREFIX = 'pre';
+
     /**
      * @type Psr6SessionHandler
      */
     private $handler;
 
     /**
-     * @type m\MockInterface|CacheItemPoolInterface
+     * @type \PHPUnit_Framework_MockObject_MockObject|CacheItemPoolInterface
      */
-    private $mock;
-
-    /**
-     * @type m\MockInterface|CacheItemInterface
-     */
-    private $itemMock;
+    private $psr6;
 
     protected function setUp()
     {
         parent::setUp();
-
-        $this->mock    = m::mock(CacheItemPoolInterface::class);
-        $this->handler = new Psr6SessionHandler($this->mock);
-
-        $this->itemMock = m::mock(CacheItemInterface::class);
-    }
-
-    public function testConstructor()
-    {
-        $this->assertInstanceOf(Psr6SessionHandler::class, $this->handler);
+        $this->psr6 = $this->getMockBuilder(ArrayCachePool::class)
+            ->setMethods(['getItem', 'deleteItem', 'save'])
+            ->getMock();
+        $this->handler = new Psr6SessionHandler($this->psr6, ['prefix' => self::PREFIX, 'ttl' => self::TTL]);
     }
 
     public function testOpen()
@@ -66,15 +59,71 @@ class Psr6SessionHandlerTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->handler->gc(4711));
     }
 
-    public function testRead()
+    public function testReadMiss()
     {
+        $item = $this->getItemMock();
+        $item->expects($this->once())
+            ->method('isHit')
+            ->willReturn(false);
+        $this->psr6->expects($this->once())
+            ->method('getItem')
+            ->willReturn($item);
+        $this->assertEquals('', $this->handler->read('foo'));
+    }
+
+    public function testReadHit()
+    {
+        $item = $this->getItemMock();
+        $item->expects($this->once())
+            ->method('isHit')
+            ->willReturn(true);
+        $item->expects($this->once())
+            ->method('get')
+            ->willReturn('bar');
+        $this->psr6->expects($this->once())
+            ->method('getItem')
+            ->willReturn($item);
+        $this->assertEquals('bar', $this->handler->read('foo'));
     }
 
     public function testWrite()
     {
+        $item = $this->getItemMock();
+        $item->expects($this->once())
+            ->method('set')
+            ->with('session value')
+            ->willReturnSelf();
+        $item->expects($this->once())
+            ->method('expiresAfter')
+            ->with(self::TTL)
+            ->willReturnSelf();
+        $this->psr6->expects($this->once())
+            ->method('getItem')
+            ->with(self::PREFIX.'foo')
+            ->willReturn($item);
+        $this->psr6->expects($this->once())
+            ->method('save')
+            ->with($item)
+            ->willReturn(true);
+        $this->assertTrue($this->handler->write('foo', 'session value'));
     }
 
     public function testDestroy()
     {
+        $this->psr6->expects($this->once())
+            ->method('deleteItem')
+            ->with(self::PREFIX.'foo')
+            ->willReturn(true);
+        $this->assertTrue($this->handler->destroy('foo'));
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getItemMock()
+    {
+        return $this->getMockBuilder(CacheItemInterface::class)
+            ->setMethods(['isHit', 'getKey', 'get', 'set', 'expiresAt', 'expiresAfter'])
+            ->getMock();
     }
 }
