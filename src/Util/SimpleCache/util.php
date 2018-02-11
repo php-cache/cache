b9b8f5b3a54ab8fce34da10e9eff37d6022abb0b
RJ Garcia <rj@bighead.net>
2018-02-11 05:32:07 -0500

    Added Cache Util Package (#206)
    * Added Cache Util Package
    
    - Created new Util repo
    - Add `remember` function
    - Added src/inc.php for including static files
    
    Signed-off-by: RJ Garcia <rj@bighead.net>
    
    * Removing snake case in variable parameter
    
    Signed-off-by: RJ Garcia <rj@bighead.net>

---
+++ src/Util/SimpleCache/util.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of php-cache organization.
+ *
+ * (c) 2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\Util\SimpleCache;
+
+use Psr\SimpleCache\CacheInterface;
+
+function remember(CacheInterface $cache, $key, $ttl, callable $createResult)
+{
+    $res = $cache->get($key);
+    if ($res) {
+        return $res;
+    }
+
+    $res = $createResult();
+    $cache->set($key, $res, $ttl);
+
+    return $res;
+}
