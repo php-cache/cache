26c6f5b7aeddbc85c22ba7f2280ed8eb033ecfc0
Daniel Bannert <d.bannert@anolilab.de>
2018-03-26 21:29:42 +0200

    fix script

--- build/components_phpunit.sh
+++ build/components_phpunit.sh
@@ -6,15 +6,14 @@ source ./build/tfold.sh
 COMPONENTS=$(find src -mindepth 2 -type f -name phpunit.xml.dist -printf '%h\n')
 
 try
-    echo -e '{\n"minimum-stability":"dev"}' > composer.json
-    php ./build/travis/build_packages.php HEAD^ $COMPONENTS
-
     if [[ "$DEPS" = "high" ]]; then
-        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP && $TEST --verbose'"
+        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP && $PHPUNIT_X'"
     elif [[ "$DEPS" = "low" ]]; then
-        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP --prefer-lowest --prefer-stable && $TEST --verbose'"
+        sleep 3
+        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP --prefer-lowest --prefer-stable && $PHPUNIT_X'"
     else
         echo "$COMPONENTS" | parallel --gnu "tfold {} $PHPUNIT_X {}"
+
         tfold tty-group $PHPUNIT --group tty
     fi
 catch || {
