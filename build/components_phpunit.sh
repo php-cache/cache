d74ff4086b03e6174a4ba24c5067043543ad6bf5
Daniel Bannert <d.bannert@anolilab.de>
2018-03-26 23:38:30 +0200

    updated the phpunit.xml

--- build/components_phpunit.sh
+++ build/components_phpunit.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
-source ./build/try_catch.sh
 source ./build/tfold.sh
 
 COMPONENTS=$(find src -mindepth 2 -type f -name phpunit.xml.dist -printf '%h\n')
 
-try
-    if [[ "$DEPS" = "high" ]]; then
-        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP && $PHPUNIT_X'"
-    elif [[ "$DEPS" = "low" ]]; then
-        sleep 3
-        echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP --prefer-lowest --prefer-stable && $PHPUNIT_X'"
-    else
-        $COMPOSER_UP
+set -e
 
-        echo "$COMPONENTS" | parallel --gnu "tfold {} $PHPUNIT_X {}"
+if [[ "$DEPS" = "high" ]]; then
+    echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP && $PHPUNIT_X'"
+elif [[ "$DEPS" = "low" ]]; then
+    sleep 3
+    echo "$COMPONENTS" | parallel --gnu -j10% "tfold {} 'cd {} && $COMPOSER_UP --prefer-lowest --prefer-stable && $PHPUNIT_X'"
+else
+    $COMPOSER_UP
 
-        tfold tty-group $PHPUNIT --group tty
-    fi
-catch || {
-    exit 1
-}
+    echo "$COMPONENTS" | parallel --gnu "tfold {} $PHPUNIT_X {}"
+
+    tfold tty-group $PHPUNIT --group tty
+fi
