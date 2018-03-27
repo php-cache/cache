f16b51d2d4cc3593ccf917cbe4e1753c143191d7
Daniel Bannert <d.bannert@anolilab.de>
2018-03-28 00:05:09 +0200

    making the build easier

--- build/php/7.0/MongoDB.sh
+++ build/php/7.0/MongoDB.sh
@@ -3,4 +3,4 @@
 echo "Install MongoDB"
 pecl install -f mongodb-1.1.2
 
-echo "extension=mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
--- build/php/7.2/MongoDB.sh
+++ build/php/7.2/MongoDB.sh
@@ -3,4 +3,4 @@
 echo "Install MongoDB"
 pecl install -f mongodb-1.4.2
 
-echo "extension=mongo" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
