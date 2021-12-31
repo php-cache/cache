9cac7f3d604f0b306376d0b9d9150337f7b1e00e
Nyholm <tobias.nyholm@gmail.com>
2021-12-31 10:42:06 +0100

    Drop support for psr/cache: v3

--- src/TagInterop/Changelog.md
+++ src/TagInterop/Changelog.md
@@ -2,10 +2,10 @@
 
 The change log describes what is "Added", "Removed", "Changed" or "Fixed" between each release.
 
-## 1.0.2
+## 1.1.0
 
 * Support PHP 8.1
-* Update dependencies
+* Support for psr/cache v2
 
 ## 1.0.1
 
--- src/TagInterop/TaggableCacheItemPoolInterface.php
+++ src/TagInterop/TaggableCacheItemPoolInterface.php
@@ -11,13 +11,8 @@
 
 namespace Cache\TagInterop;
 
-use Psr\Cache\CacheItemInterface;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Cache\InvalidArgumentException;
-
-$r = new \ReflectionClass(CacheItemPoolInterface::class);
-$m = $r->getMethod('getItem');
-$param = $m->getParameters()[0];
 
 /**
  * Interface for invalidating cached items using tags. This interface is a soon-to-be-PSR.
@@ -25,85 +20,41 @@ $param = $m->getParameters()[0];
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  * @author Nicolas Grekas <p@tchwork.com>
  */
+interface TaggableCacheItemPoolInterface extends CacheItemPoolInterface
+{
+    /**
+     * Invalidates cached items using a tag.
+     *
+     * @param string $tag The tag to invalidate
+     *
+     * @throws InvalidArgumentException When $tags is not valid
+     *
+     * @return bool True on success
+     */
+    public function invalidateTag($tag);
 
-if ($param->getType() === null) {
-    // definition for psr/cache 1.0.* version
-    interface TaggableCacheItemPoolInterface extends CacheItemPoolInterface
-    {
-        /**
-         * Invalidates cached items using a tag.
-         *
-         * @param string $tag The tag to invalidate
-         *
-         * @throws InvalidArgumentException When $tags is not valid
-         *
-         * @return bool True on success
-         */
-        public function invalidateTag($tag);
+    /**
+     * Invalidates cached items using tags.
+     *
+     * @param string[] $tags An array of tags to invalidate
+     *
+     * @throws InvalidArgumentException When $tags is not valid
+     *
+     * @return bool True on success
+     */
+    public function invalidateTags(array $tags);
 
-        /**
-         * Invalidates cached items using tags.
-         *
-         * @param string[] $tags An array of tags to invalidate
-         *
-         * @throws InvalidArgumentException When $tags is not valid
-         *
-         * @return bool True on success
-         */
-        public function invalidateTags(array $tags);
+    /**
+     * {@inheritdoc}
+     *
+     * @return TaggableCacheItemInterface
+     */
+    public function getItem($key);
 
-        /**
-         * {@inheritdoc}
-         *
-         * @return TaggableCacheItemInterface
-         */
-        public function getItem($key): CacheItemInterface;
-
-        /**
-         * {@inheritdoc}
-         *
-         * @return array|\Traversable|TaggableCacheItemInterface[]
-         */
-        public function getItems(array $keys = []);
-    }
-} else {
-    // since 2.0 version of psr/cache was changed signature
-    interface TaggableCacheItemPoolInterface extends CacheItemPoolInterface
-    {
-        /**
-         * Invalidates cached items using a tag.
-         *
-         * @param string $tag The tag to invalidate
-         *
-         * @return bool True on success
-         * @throws InvalidArgumentException When $tags is not valid
-         *
-         */
-        public function invalidateTag($tag);
-
-        /**
-         * Invalidates cached items using tags.
-         *
-         * @param string[] $tags An array of tags to invalidate
-         *
-         * @return bool True on success
-         * @throws InvalidArgumentException When $tags is not valid
-         *
-         */
-        public function invalidateTags(array $tags);
-
-        /**
-         * {@inheritdoc}
-         *
-         * @return TaggableCacheItemInterface
-         */
-        public function getItem(string $key): CacheItemInterface;
-
-        /**
-         * {@inheritdoc}
-         *
-         * @return array|\Traversable|TaggableCacheItemInterface[]
-         */
-        public function getItems(array $keys = []): iterable;
-    }
+    /**
+     * {@inheritdoc}
+     *
+     * @return array|\Traversable|TaggableCacheItemInterface[]
+     */
+    public function getItems(array $keys = []);
 }
