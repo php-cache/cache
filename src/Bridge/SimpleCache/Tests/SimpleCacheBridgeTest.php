fb0b5a878107f5c1f8da5f4ca42564e80afc8296
Ruud Kamphuis <ruudk@users.noreply.github.com>
2022-01-03 14:51:42 +0100

    Drop PHP 5.6 + 7.0 (#257)
    * Drop PHP 5.6 + 7.0
    
    * Remove `syntaxCheck`
    
    * Add void

--- src/Bridge/SimpleCache/Tests/SimpleCacheBridgeTest.php
+++ src/Bridge/SimpleCache/Tests/SimpleCacheBridgeTest.php
@@ -34,7 +34,7 @@ class SimpleCacheBridgeTest extends TestCase
      */
     private $itemMock;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
